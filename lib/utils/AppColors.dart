a77aa07a8db0f9d1ce54ce032b22eacfcda98478
muzzuaspira7 <muzzuaspira7@gmail.com>
2024-02-17 19:47:57 +0530

    first commit

---
+++ lib/utils/AppColors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color appBarColor = Color(0xFF424242);
+  static const Color containerColor = Color(0xFFFFFFFF);
+  static const Color primaryTextColor = Color(0xFF424242);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color cardBackgroundColor = Color(0xFFE0E0E0);
+  static const Color buttonColor = Color(0xFF4CAF50);
+  static const Color buttonTextColor = Color(0xFFFFFFFF);
+}
